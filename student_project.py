3866546902eb1080cae679f2d893e7a29dc8d045
FleetingSheep <jackliam5257@gmail.com>
2024-06-24 11:30:05 -0500

    Add files via upload

--- student_project.py
+++ student_project.py
@@ -1,3 +1,20 @@
+from datetime import datetime
+
+'''
+function to write error log file
+input: error message
+output: none
+'''
+
+def write_to_error_log(error_message):
+    try:
+        log_file = open("error_log.txt", "a")
+        log_file.write(f"{datetime.now()}, {error_message}\n\n")
+        log_file.close()
+    except Exception as e:
+        print(e)
+        return
+
 
 students_list = []
 student_file = open("students.csv")
@@ -14,7 +31,7 @@ class Student:
         self.get_class_level()
 
     def __str__(self):
-        return f"{self._class} {self._fname} {self._lname}:\n\nMajor: {self._major}\nCredit Hours: {self._ch}\nGPA: {self._gpa}\nID: {self._id}\n"
+        return f"{self._class} {self._fname} {self._lname}:\n\nMajor: {self._major}, Credit Hours: {self._ch}\nGPA: {self._gpa}, ID: {self._id}\n"
 
     def get_class_level(self):
 
@@ -33,12 +50,22 @@ class Student:
 
 def get_students():
 
-    for line in student_file:
-        x = line.split(",")
-        if x[0] == "first_name":
-            continue
-        x = Student(x[0], x[1], x[2], x[3], x[4], x[5])
-        students_list.append(x)
+    for line_number, line in enumerate(student_file):
+        try:
+            if line_number == 0:
+                continue
+            x = line.split(",")
+            if len(x) > 6:
+                write_to_error_log(f"Too many entries on line {line_number}")
+                continue
+            elif len(x) < 6:
+                write_to_error_log(f"Too few entries on line {line_number}")
+                continue
+            else:
+                x = Student(x[0], x[1], x[2], x[3], x[4], x[5])
+                students_list.append(x)
+        except:
+            write_to_error_log(f"Unknown error on line {line_number}")
     for student in students_list:
         print(student)
 
